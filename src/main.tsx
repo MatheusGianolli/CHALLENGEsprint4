144ef224a67ae5108d8917aa6cadc5a0c9fc5e8b
Matheus Gianolli <matheusgianolli@hotmail.com>
2025-11-06 17:52:15 -0300

    feat:Site funcionando e rodando porem antes tem alguns ajustes a ser feito na API

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+// 1. --- ADICIONE ESTA IMPORTAÇÃO ---
+import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';// Garanta que esta linha exista!
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* 2. --- ENVOLVA SEU APP COM O <BrowserRouter> --- */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
